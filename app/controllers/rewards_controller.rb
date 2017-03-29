c98996945d73eccf55e2ba31e771b4a8df78a2a2
MFigand <m.figand@gmail.com>
2017-03-29 22:39:01 +0200

    Pass the correct params in new rewards form

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,57 @@
 class RewardsController < ApplicationController
+
+  def index
+    @company = Company.find(params[:company_id])
+    @rewards = @company.rewards
+  end
+
+  def reset_filterrific
+    session[:filterrific_rewards] = nil
+    redirect_to action: :index
+  end
+
+  def all_rewards
+    @allRewards = Reward.all
+    @filterrific = initialize_filterrific(
+      Reward,
+      params[:filterrific]
+    ) or return
+    @all_rewards = @filterrific.find.page(params[:page])
+  end
+
+  def new
+    @branch = Branch.find(params[:branch_id])
+    @reward = Reward.new
+  end
+
+  def create
+    #@company = Company.find(params[:company_id])
+    @reward = Reward.new reward_params
+    @reward.company_id = params[:company_id]
+    if @reward.save
+      flash[:notice] = "Reward created succesfully"
+      redirect_to company_rewards_path(current_company.id,params[:id])
+    else
+      flash[:alert] = "ALERT Reward not created"
+      render 'new'
+    end
+  end
+
+  def show
+    @company = Company.find(params[:company_id])
+    @reward = Reward.find(params[:id])
+  end
+
+  def destroy
+    @reward = Reward.find(params[:id])
+    @reward.destroy
+    redirect_to company_rewards_path(current_company.id,params[:id])
+  end
+
+  private
+
+  def reward_params
+   params.require(:reward).permit(:name, :kms_cost, :description, :valid_from, :valid_through, :available_units, :image)
+  end
+
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -8,4 +8,53 @@ class Reward < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 15}, format: {with: /\A[a-zA-ZÑñ0-9\ ]+\z/}
   validates :kms_cost, :available_units, :description, :valid_from, :valid_through, :image, presence: true
 
+  has_attached_file :image, styles: { medium: "300x300>", small: "200x200!", xsmall: "150x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  filterrific(
+    default_filter_params: { sorted_by: 'created_at_desc' },
+    available_filters: [:sorted_by, :search_query])
+
+  scope :sorted_by, lambda { |sort_option|
+  direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+  case sort_option.to_s
+  when /^created_at_/
+
+    order("rewards.created_at #{ direction }")
+  else
+    raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+  end
+}
+
+  scope :with_city, lambda { |city|
+    where(city: [*city])
+  }
+
+  scope :search_query, lambda { |query|
+  return nil  if query.blank?
+  return nil if query.class == Fixnum
+
+  terms = query.downcase.split(/\s+/)
+
+  terms = terms.map { |e|
+    (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+  }
+
+  num_or_conds = 2
+  where(
+    terms.map { |term|
+      "(LOWER(rewards.name) LIKE ? OR LOWER(rewards.name) LIKE ?)"
+    }.join(' AND '),
+    *terms.map { |e| [e] * num_or_conds }.flatten
+  )
+}
+
+def self.options_for_sorted_by
+    [
+      ['Name (a-z)', 'name_asc'],
+      ['Registration date (newest first)', 'created_at_desc'],
+      ['Registration date (oldest first)', 'created_at_asc'],
+    ]
+  end
+
 end
